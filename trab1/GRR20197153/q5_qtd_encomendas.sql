1d8828d7e08256480f3d7665a75beeb0f33a2a1b
Arthur Vilar <ahc.vilar@gmail.com>
2022-06-28 17:15:50 -0300

    5-6 done

--- trab1/GRR20197153/q5_qtd_encomendas.sql
+++ trab1/GRR20197153/q5_qtd_encomendas.sql
@@ -1,31 +1,10 @@
 select 
-    N_NAME, QTD_ENCOMENDAS 
-from 
-(
-    select 
-        C_NATIONKEY, count(*) as QTD_ENCOMENDAS 
-    from 
-    (
-        select 
-            O_CUSTKEY, C_NATIONKEY 
-        from 
-            ORDERS 
-        inner join CUSTOMER on 
-            ORDERS.O_CUSTKEY = CUSTOMER.C_CUSTKEY 
-        group by 
-            O_CUSTKEY
-    ) 
-    group by 
-        C_NATIONKEY
-) 
-inner join NATION on 
-    C_NATIONKEY = NATION.N_NATIONKEY 
-group by 
-    N_NAME 
-order by 
-    QTD_ENCOMENDAS desc;
-
-------------------------------------------------------------------------------------------------------------------------------------------
-select O_CUSTKEY, count(*) from (select O_ORDERKEY, O_CUSTKEY from ORDERS order by O_CUSTKEY) group by O_CUSTKEY;
-
-select C_NATIONKEY, PEDIDOS from (select O_CUSTKEY, count(*) as PEDIDOS from (select O_ORDERKEY, O_CUSTKEY from ORDERS order by O_CUSTKEY) group by O_CUSTKEY) inner join CUSTOMER on O_CUSTKEY = CUSTOMER.C_CUSTKEY;
+    NATION.N_NAME,
+    count(ORDERS.O_ORDERKEY) as PEDIDOS     
+from ORDERS 
+join CUSTOMER 
+    on ORDERS.O_CUSTKEY = CUSTOMER.C_CUSTKEY 
+join NATION 
+    on CUSTOMER.C_NATIONKEY = NATION.N_NATIONKEY 
+group by NATION.N_NAME 
+order by PEDIDOS desc;

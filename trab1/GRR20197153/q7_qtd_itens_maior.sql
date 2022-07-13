b3bbb6de14c8b4bb8eb7af76a7cf31b1401a8270
Arthur Vilar <ahc.vilar@gmail.com>
2022-07-13 15:17:40 -0300

    7 done

--- trab1/GRR20197153/q7_qtd_itens_maior.sql
+++ trab1/GRR20197153/q7_qtd_itens_maior.sql
@@ -1,20 +1,3 @@
-select N_NAME, QTD
-from (
-    select 
-        N_NAME, 
-        sum(L_QUANTITY) as QTD 
-    from ORDERS 
-    join LINEITEM 
-        on LINEITEM.L_ORDERKEY = ORDERS.O_ORDERKEY 
-    join CUSTOMER 
-        on CUSTOMER.C_CUSTKEY = ORDERS.O_CUSTKEY 
-    join NATION 
-        on NATION.N_NATIONKEY = CUSTOMER.C_NATIONKEY 
-    group by N_NAME
-)
-where QTD > 62000
-order by QTD desc;
-  
 select n_name, sum(l_quantity) as qtd 
 from orders, lineitem, customer, nation 
 where l_orderkey = o_orderkey 

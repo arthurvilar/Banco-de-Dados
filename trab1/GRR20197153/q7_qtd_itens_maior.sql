4bf4dd7f49c0a1d08f359b98d087f8ef6040ef0f
Arthur Vilar <ahc.vilar@gmail.com>
2022-07-13 14:27:39 -0300

    UPDATE q7

--- trab1/GRR20197153/q7_qtd_itens_maior.sql
+++ trab1/GRR20197153/q7_qtd_itens_maior.sql
@@ -14,3 +14,12 @@ from (
 )
 where QTD > 62000
 order by QTD desc;
+  
+select n_name, sum(l_quantity) as qtd 
+from orders, lineitem, customer, nation 
+where l_orderkey = o_orderkey 
+    and c_custkey = o_custkey 
+    and n_nationkey = c_nationkey 
+group by n_name 
+having qtd > 62000 
+order by qtd desc;
